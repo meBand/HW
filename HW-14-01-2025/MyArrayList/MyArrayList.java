ad9a3192f3cd446a27abdf39eba69f7ed18b2464
Kirill Gudov <gudov.kir@gmail.com>
2024-12-29 17:29:55 +0300

    add addByIndex, addInside, refact

--- HW-14-01-2025/MyArrayList/MyArrayList.java
+++ HW-14-01-2025/MyArrayList/MyArrayList.java
@@ -21,10 +21,18 @@ public class MyArrayList<T> {
     private final int DEFAULT_CAPACITY = 10;
 
     /**
+     * Create new array T[]
+     * @param capacity initial capacity
+     */
+    private T[] createArray(int capacity) {
+        return (T[]) new Object[capacity];
+    }
+
+    /**
      * Default constructor {@link MyArrayList} with no parameters and default(10) initial capacity
      */
     public MyArrayList() {
-        elements = (T[]) new Object[DEFAULT_CAPACITY];
+        elements = createArray(DEFAULT_CAPACITY);
         size = 0;
     }
 
@@ -34,9 +42,9 @@ public class MyArrayList<T> {
      */
     public MyArrayList(int capacity) {
         if ( capacity > 0 ) {
-            elements = (T[]) new Object[capacity];
+            elements = createArray(capacity);
         } else if ( capacity == 0 ) {
-            elements = (T[]) new Object[DEFAULT_CAPACITY];
+            elements = createArray(DEFAULT_CAPACITY);
         } else {
             throw new IllegalArgumentException();
         }
@@ -48,18 +56,6 @@ public class MyArrayList<T> {
      */
     public int size() {
         return size;
-    }
-
-    /**
-     * Ensures that the internal array has enough capacity to accommodate new elements.
-     */
-    private void ensureCapacity() {
-        if (size >= elements.length) {
-            int newCapacity = (int) (elements.length * 1.5);
-            T[] newArray = (T[]) new Object[newCapacity];
-            System.arraycopy(elements, 0, newArray, 0, elements.length);
-            elements = newArray;
-        }
     }
 
     /**
@@ -91,7 +87,7 @@ public class MyArrayList<T> {
     private void addLast(T element) {
         if (size >= elements.length) {
             int newCapacity = (int) (elements.length * 1.5);
-            T[] newArray = (T[]) new Object[newCapacity];
+            T[] newArray = createArray(newCapacity);
             System.arraycopy(elements, 0, newArray, size, elements.length);
             elements = newArray;
         }
@@ -105,17 +101,18 @@ public class MyArrayList<T> {
     private void addFirst(T element) {
         if (size >= elements.length) {
             int newCapacity = (int) (elements.length * 1.5);
-            T[] newArray = (T[]) new Object[newCapacity];
+            T[] newArray = createArray(newCapacity);
             newArray[size++] = element;
             System.arraycopy(elements, 0, newArray, size, elements.length);
             elements = newArray;
         } else {
-            T[] newArray = (T[]) new Object[elements.length];
+            T[] newArray = createArray(elements.length);
             newArray[size++] = element;
             System.arraycopy(elements, 0, newArray, size, elements.length);
             elements = newArray;
         }
     }
+    //todo addFirst addLast - вынести метод
 
     /**
      * Adds an element to the list by index.
@@ -128,7 +125,26 @@ public class MyArrayList<T> {
         } else if ( index == 0 ) {
             addFirst(element);
         } else {
-            //todo
+            if ( size >= elements.length ) {
+                int newCapacity = (int) (elements.length * 1.5);
+                addInside(index, element, newCapacity);
+            } else {
+                addInside(index, element, elements.length);
+            }
         }
+    }
+
+    /**
+     * Adds an element to the list by index.
+     * @param index
+     * @param element the element to add
+     */
+    private void addInside(int index, T element, int capacity) {
+        T[] newArray = createArray(capacity);
+        System.arraycopy(elements, 0, newArray, size, elements.length - index);
+        newArray[size++] = element;
+        System.arraycopy(elements, elements.length - index + 1,
+                newArray, index + 1, elements.length);
+        elements = newArray;
     }
 }
