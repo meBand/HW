b474d8fc428004dab0569396bf7b964ee815dc3d
Kirill Gudov <gudov.kir@gmail.com>
2025-01-08 20:40:14 +0300

    add paramResolver

---
+++ src/test/java/HW_14_01_25/paramResolver/ListParamResolver.java
@@ -0,0 +1,20 @@
+package HW_14_01_25.paramResolver;
+
+import HW_14_01_25.MyLinkedList.MyLinkedList;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class ListParamResolver implements ParameterResolver {
+
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter().getType() == MyLinkedList.class;
+    }
+
+    @Override
+    public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return new MyLinkedList<>();
+    }
+}
