38ddb6347ae849f44c4b613bf58a13a0bdc753b6
Kirill Gudov <gudov.kir@gmail.com>
2025-01-08 15:47:16 +0300

    refact MyLinkedList

--- src/main/java/HW_14_01_25/MyLinkedList/MyLinkedList.java
+++ src/main/java/HW_14_01_25/MyLinkedList/MyLinkedList.java
@@ -1,7 +1,5 @@
 package HW_14_01_25.MyLinkedList;
 
-import java.lang.reflect.Array;
-
 /**
  * MyLinkedList is a simple implementation of linked list.
  * It allows for adding, removing, and accessing elements.
