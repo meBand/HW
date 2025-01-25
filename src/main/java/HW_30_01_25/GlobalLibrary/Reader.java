70cc0b49d76fc1b473390a91305339c4ca612c14
Kirill Gudov <gudov.kir@gmail.com>
2025-01-25 11:53:52 +0300

    add javadoc to reader

--- src/main/java/HW_30_01_25/GlobalLibrary/Reader.java
+++ src/main/java/HW_30_01_25/GlobalLibrary/Reader.java
@@ -3,18 +3,42 @@ package HW_30_01_25.GlobalLibrary;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Reader implements interface Subscriber
+ * Just person who likes to read books
+ * Has its own collection of books and can subscribe to library updates
+ */
 public class Reader implements Subscriber {
+    /**
+     * Reader name
+     */
     private String name;
+
+    /**
+     * Readers collection of the books
+     */
     private List<Book> books = new ArrayList<>();
 
+    /**
+     * Constructor reader
+     * @param name {link #name}
+     */
     public Reader(String name) {
         this.name = name;
     }
 
+    /**
+     * Method returns reader name
+     * @return {@link #name}
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Method updates the user's collection by adding a new book
+     * @param book {@link Book}
+     */
     @Override
     public void update(Book book) {
         books.add(book);
