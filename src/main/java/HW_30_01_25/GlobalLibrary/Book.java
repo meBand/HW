4199308e1079035f01713b98bbedd6dfd63b9eea
Kirill Gudov <gudov.kir@gmail.com>
2025-01-25 11:21:34 +0300

    add javadoc to Book

--- src/main/java/HW_30_01_25/GlobalLibrary/Book.java
+++ src/main/java/HW_30_01_25/GlobalLibrary/Book.java
@@ -8,17 +8,33 @@ public class Book {
      * {@link Book} title
      */
     private final String title;
+    /**
+     * {@link Book} genre
+     */
     private final Genre genre;
 
+    /**
+     * Constructor of {@link Book}
+     * @param title {@link Book} title
+     * @param genre {@link Book} genre
+     */
     public Book(String title, Genre genre) {
         this.title = title;
         this.genre = genre;
     }
 
+    /**
+     * Method returns book title
+     * @return {@link #title}
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Method returns book genre
+     * @return {@link #genre}
+     */
     public Genre getGenre() {
         return genre;
     }
