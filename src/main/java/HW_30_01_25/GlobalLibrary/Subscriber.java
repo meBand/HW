f18f5f590900f16b67f075bf627b2eee4f74c4fa
Kirill Gudov <gudov.kir@gmail.com>
2025-01-25 11:30:50 +0300

    add javadoc to subscriber

--- src/main/java/HW_30_01_25/GlobalLibrary/Subscriber.java
+++ src/main/java/HW_30_01_25/GlobalLibrary/Subscriber.java
@@ -1,6 +1,18 @@
 package HW_30_01_25.GlobalLibrary;
 
+/**
+ * Interface Subscriber
+ */
 public interface Subscriber {
+    /**
+     * Method updates the subscriber state when a new book arrives
+     * @param book {@link Book}
+     */
     void update(Book book);
+
+    /**
+     * Method returns name subscriber
+     * @return String name
+     */
     String getName();
 }
